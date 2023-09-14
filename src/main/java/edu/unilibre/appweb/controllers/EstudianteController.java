104d1834730de5104ed7ad7232de59110d326827
María Fernanda Umbarila Suárez <fernandaumbarila@gmail.com>
2023-09-14 10:52:38 -0500

    Endpoints completos

--- src/main/java/edu/unilibre/appweb/controllers/EstudianteController.java
+++ src/main/java/edu/unilibre/appweb/controllers/EstudianteController.java
@@ -1,8 +1,11 @@
 package edu.unilibre.appweb.controllers;
+import edu.unilibre.appweb.models.EstudianteModel;
+import org.springframework.http.HttpStatus;
 import edu.unilibre.appweb.repository.EstudianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/")
@@ -10,5 +13,50 @@ public class EstudianteController {
 
     @Autowired
     private EstudianteRepository estudianteRespository;
+
+    @PostMapping("/create_student")
+    public ResponseEntity<EstudianteModel> createEstudiante(@RequestBody EstudianteModel estudiante){
+        //Utiliza el JPA para
+        EstudianteModel nuevoEstudiante =  estudianteRespository.save(estudiante);
+        return new ResponseEntity<>(nuevoEstudiante, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get_student/{id}")
+    public Optional<EstudianteModel> getEstudiante(@PathVariable Integer id) {
+        return estudianteRespository.findById(id);
+    }
+
+    @PutMapping("/update_student/{id}")
+    public ResponseEntity<EstudianteModel> updateEstudiante(@PathVariable Integer id, @RequestBody EstudianteModel estudiante) {
+
+        //Verifica que el estudiante exista primero
+        Optional<EstudianteModel> estudianteActual = estudianteRespository.findById(id);
+        boolean estudianteExiste = estudianteActual.isPresent();
+
+        if (!estudianteExiste) {
+            return new ResponseEntity<EstudianteModel>(HttpStatus.NOT_FOUND);
+        }else{
+            estudianteActual.get().setNombre(estudiante.getNombre());
+            estudianteActual.get().setApellido(estudiante.getApellido());
+            estudianteActual.get().setCorreo(estudiante.getCorreo());
+            estudianteRespository.save(estudianteActual.get());
+            return new ResponseEntity<EstudianteModel>(HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/delete_student/{id}")
+    public ResponseEntity<HttpStatus> borraEstudiante(@PathVariable Integer id) {
+
+        //Verifica que el estudiante exista primero
+        Optional<EstudianteModel> estudianteActual = estudianteRespository.findById(id);
+        boolean estudianteExiste = estudianteActual.isPresent();
+
+        if (!estudianteExiste) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            estudianteRespository.delete(estudianteActual.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
 
 }
