de65519f9af81fd04b6f7a1e2ee7c860c62c0cd3
Dasomacha <110133166+Dasomacha@users.noreply.github.com>
2023-10-03 00:29:14 -0500

    new

--- Frontend/JS/tabla.js
+++ Frontend/JS/tabla.js
@@ -9,13 +9,14 @@ document.addEventListener('DOMContentLoaded', function () {
             data.forEach(student => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-                  <td>${student.id}</td>
-                  <td>${student.nombre}</td>
-                  <td>${student.apellido}</td>
-                  <td>${student.correo}</td>
-                  <td> <button>Editar</button>
-                        <button>Eliminar</button>
-                </td>
+                    <td>${student.id}</td>
+                    <td>${student.nombre}</td>
+                    <td>${student.apellido}</td>
+                    <td>${student.correo}</td>
+                    <td>
+                        <i class="fa-solid fa-pen-to-square" style="color: #020203; cursor: pointer;" onclick="editarEstudiante(${student.id})"></i>
+                        <i class="fa-solid fa-trash" style="color: #0f0f10; cursor: pointer;" onclick="eliminarEstudiante(${student.id})"></i>
+                    </td>
               `;
                 studentTableBody.appendChild(row);
             });
@@ -24,3 +25,30 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Error al obtener la lista de estudiantes:', error);
         });
 });
+function editarEstudiante(id) {
+
+}
+
+function eliminarEstudiante(id) {
+    // Realiza una solicitud DELETE a tu API para eliminar el estudiante con el ID proporcionado
+    fetch(`http://localhost:8081/api/delete_student/${id}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (response.status === 204) {
+                // Éxito al eliminar, puedes actualizar la lista de estudiantes o realizar otras acciones
+                console.log('Estudiante eliminado con éxito.');
+                // Por ejemplo, puedes recargar la página actual para actualizar la lista de estudiantes
+                location.reload();
+            } else if (response.status === 404) {
+                // Estudiante no encontrado, maneja el error como desees
+                console.error('Estudiante no encontrado.');
+            } else {
+                // Otro error, maneja el error como desees
+                console.error('Error al eliminar el estudiante.');
+            }
+        })
+        .catch(error => {
+            console.error('Error al eliminar el estudiante:', error);
+        });
+}
