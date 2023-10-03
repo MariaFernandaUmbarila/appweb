fdaade794239c22ec6b912c7a989a8d41a0406b0
Dasomacha <110133166+Dasomacha@users.noreply.github.com>
2023-10-03 02:12:59 -0500

    Tabla, listar estudiantes

--- Frontend/JS/tabla.js
+++ Frontend/JS/tabla.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const studentTableBody = document.getElementById('studentTableBody');
 
-    // Realiza una solicitud GET a tu API para obtener la lista de estudiantes
     fetch('http://localhost:8081/api/get_all_students')
         .then(response => response.json())
         .then(data => {
-            // Itera sobre los datos de estudiantes y crea filas en la tabla
             data.forEach(student => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
@@ -30,21 +28,16 @@ function editarEstudiante(id) {
 }
 
 function eliminarEstudiante(id) {
-    // Realiza una solicitud DELETE a tu API para eliminar el estudiante con el ID proporcionado
     fetch(`http://localhost:8081/api/delete_student/${id}`, {
         method: 'DELETE'
     })
         .then(response => {
             if (response.status === 204) {
-                // Éxito al eliminar, puedes actualizar la lista de estudiantes o realizar otras acciones
                 console.log('Estudiante eliminado con éxito.');
-                // Por ejemplo, puedes recargar la página actual para actualizar la lista de estudiantes
                 location.reload();
             } else if (response.status === 404) {
-                // Estudiante no encontrado, maneja el error como desees
                 console.error('Estudiante no encontrado.');
             } else {
-                // Otro error, maneja el error como desees
                 console.error('Error al eliminar el estudiante.');
             }
         })
