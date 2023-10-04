2c742f32e8bccf2c4935d0c6c261faf8102aefdf
María Fernanda Umbarila Suárez <fernandaumbarila@gmail.com>
2023-10-03 20:19:09 -0500

    Terminación de la limpieza del código, endpoints funcionando

--- src/main/resources/static/JS/RegistroEstudiante.js
+++ src/main/resources/static/JS/RegistroEstudiante.js
@@ -5,22 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const editId = urlParams.get("edit");
 
-    if (editId !== null) {
-
-        fetch(`http://localhost:8081/api/get_student/${editId}`)
-        .then(response => response.json())
-        .then(data => {
-            // Llenar el formulario con los datos del estudiante obtenidos de la API
-            document.getElementById("nombre").value = data.nombre;
-            document.getElementById("apellido").value = data.apellido;
-            document.getElementById("correo").value = data.correo;
-            document.getElementById("registrarBtn").textContent = "Actualizar";
-        })
-        .catch(error => {
-            console.error("Error al obtener los datos del estudiante: ", error);
-        });
-
-    }
+    if(editId) consultarIdEstudiante(editId);
 
     document.getElementById('registroForm').addEventListener('submit', function (event) {
 
@@ -31,9 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
         let nombre = document.getElementById('nombre').value;
         let apellido = document.getElementById('apellido').value;
         let correo = document.getElementById('correo').value;
-        const nombreError = document.getElementById('nombreError');
-        const apellidoError = document.getElementById('apellidoError');
-        const emailError = document.getElementById('emailError');
 
         // Crea un objeto con los datos del estudiante en el formulario
         let datosEstudiante = {
@@ -42,79 +24,111 @@ document.addEventListener('DOMContentLoaded', function () {
             correo: correo
         };
 
-        let isValid = true;
-
-        // Validación del campo de nombre
-        if (nombre.trim() === '') {
-            isValid = false;
-            nombreError.textContent = 'El nombre es obligatorio';
-        } else {
-            nombreError.textContent = '';
-        }
-
-        // Validación del campo de apellido
-        if (apellido.trim() === '') {
-            isValid = false;
-            apellidoError.textContent = 'El apellido es obligatorio';
-        } else {
-            apellidoError.textContent = '';
-        }
-
-        // Validación del campo de correo electrónico
-        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        if (!emailPattern.test(correo)) {
-            isValid = false;
-            emailError.textContent = 'El correo electrónico no es válido';
-        } else {
-            emailError.textContent = '';
-        }
-
         // Verificar si el parámetro "edit" está presente, es decir, se quiere actualizar
         if (editId) {
-
-            // Hacer una solicitud a tu API para obtener los datos del estudiante por su ID (editId)
-            fetch(`http://localhost:8081/api/update_student/${editId}`,{
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(datosEstudiante)
-            }).then(response => {
-                if (response.status === 200) {
-                    alert('Estudiante actualizado con éxito.');
-                    window.location.href = './TablaEstudiantes.html';
-                } else if (response.status === 404) {
-                    alert('Estudiante no encontrado.');
-                    window.location.href = './TablaEstudiantes.html';
-                } else {
-                    alert('Error al actualizar el estudiante.');
-                    window.location.href = './TablaEstudiantes.html';
-                }
-            }).catch(error => {
-                alert('Error al actualizar estudiante: ' + error);
-                window.location.href = './TablaEstudiantes.html';
-            });
-
+            actualizarEstudiante(editId, datosEstudiante);
         }else{
-
-            // Consumo del endpoint de actualización, ya que no está presente el parámetro edir
-            fetch('http://localhost:8081/api/create_student', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(datosEstudiante)
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Redirige a la página de la tabla después del registro exitoso
-                // o actualiza la lista de estudiantes y muestra los datos
-                window.location.href = './TablaEstudiantes.html';
-            })
-            .catch(error => {
-                console.error('Error al registrar estudiante:', error);
-            });
+            //Si todos los campos del formulario son válidos
+            if (validarCamposForm(nombre, apellido, correo)) crearEstudiante(datosEstudiante);
         }
-
     });
 });
+
+//Función de validación de campos del formulario
+function validarCamposForm(nombreInput, apellidoInput, correoInput){
+
+    const nombreError = document.getElementById('nombreError');
+    const apellidoError = document.getElementById('apellidoError');
+    const emailError = document.getElementById('emailError');
+
+    let isValid = true;
+
+    // Validación del campo de nombre
+    if (nombreInput.trim() === '') {
+        isValid = false;
+        nombreError.textContent = 'El nombre es obligatorio';
+    } else {
+        nombreError.textContent = '';
+    }
+
+    // Validación del campo de apellido
+    if (apellidoInput.trim() === '') {
+        isValid = false;
+        apellidoError.textContent = 'El apellido es obligatorio';
+    } else {
+        apellidoError.textContent = '';
+    }
+
+    // Validación del campo de correo electrónico
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(correoInput)) {
+        isValid = false;
+        emailError.textContent = 'El correo electrónico no es válido';
+    } else {
+        emailError.textContent = '';
+    }
+
+    return isValid;
+}
+
+//Función para consulta del id trapido del parámetro
+function consultarIdEstudiante(idParam){
+    fetch(`http://localhost:8081/api/get_student/${idParam}`)
+    .then(response => response.json())
+    .then(data => {
+        // Llenar el formulario con los datos del estudiante obtenidos de la API
+        document.getElementById("nombre").value = data.nombre;
+        document.getElementById("apellido").value = data.apellido;
+        document.getElementById("correo").value = data.correo;
+        document.getElementById("registrarBtn").textContent = "Actualizar";
+    })
+    .catch(error => {
+        console.error("Error al obtener los datos del estudiante: ", error);
+    });
+}
+
+//Función para actualización del estudiante
+function actualizarEstudiante(idParam, body){
+    // Hacer una solicitud a tu API para obtener los datos del estudiante por su ID (editId)
+    fetch(`http://localhost:8081/api/update_student/${idParam}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }).then(response => {
+        if (response.status === 200) {
+            alert('Estudiante actualizado con éxito.');
+            window.location.href = './TablaEstudiantes.html';
+        } else if (response.status === 404) {
+            alert('Estudiante no encontrado.');
+            window.location.href = './TablaEstudiantes.html';
+        } else {
+            alert('Error al actualizar el estudiante.');
+            window.location.href = './TablaEstudiantes.html';
+        }
+    }).catch(error => {
+        alert('Error al actualizar estudiante: ' + error);
+        window.location.href = './TablaEstudiantes.html';
+    });
+}
+
+//Función para creación del estudiante
+function crearEstudiante(body){
+    // Consumo del endpoint de actualización, ya que no está presente el parámetro edir
+    fetch('http://localhost:8081/api/create_student', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+    .then(response => response.json())
+    .then(() => {
+        // Redirige a la página de la tabla después del registro exitoso o actualiza la lista de estudiantes y muestra los datos
+        window.location.href = './TablaEstudiantes.html';
+    })
+    .catch(error => {
+        console.error('Error al registrar estudiante:', error);
+    });
+}
--- src/main/resources/static/JS/TablaEstudiantes.js
+++ src/main/resources/static/JS/TablaEstudiantes.js
@@ -1,35 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
+    listarEstudiantes();
+});
+
+//Función de listado de estudiantes
+function listarEstudiantes(){
 
     const studentTableBody = document.getElementById('studentTableBody');
 
     fetch('http://localhost:8081/api/get_all_students')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(student => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${student.id}</td>
-                    <td>${student.nombre}</td>
-                    <td>${student.apellido}</td>
-                    <td>${student.correo}</td>
-                    <td>
-                        <i class="fa-solid fa-pen-to-square" style="color: #020203; cursor: pointer;" onclick="editarEstudiante('${student.id}')"></i>
-                        <i class="fa-solid fa-trash" style="color: #0f0f10; cursor: pointer;" onclick="eliminarEstudiante(${student.id})"></i>
-                    </td>
-              `;
-                studentTableBody.appendChild(row);
-            });
-        })
-        .catch(error => {
-            alert('Error al obtener la lista de estudiantes: ' + error);
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(student => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${student.id}</td>
+                <td>${student.nombre}</td>
+                <td>${student.apellido}</td>
+                <td>${student.correo}</td>
+                <td>
+                    <i class="fa-solid fa-pen-to-square" style="color: #020203; cursor: pointer;" onclick="editarEstudiante('${student.id}')"></i>
+                    <i class="fa-solid fa-trash" style="color: #0f0f10; cursor: pointer;" onclick="eliminarEstudiante(${student.id})"></i>
+                </td>
+          `;
+            studentTableBody.appendChild(row);
         });
-});
+    })
+    .catch(error => {
+        alert('Error al obtener la lista de estudiantes: ' + error);
+    });
+}
 
+//Redirección para las funciones de editar estudiante
 function editarEstudiante(student_edit) {
     // Redirigir a la página RegistroEstudiantes.html?edit=id
     window.location.href = `./RegistroEstudiantes.html?edit=${student_edit}`;
 }
 
+//Consumo del endpoint para eliminación
 function eliminarEstudiante(id) {
     fetch(`http://localhost:8081/api/delete_student/${id}`, {
         method: 'DELETE'
